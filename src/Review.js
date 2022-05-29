12c2fd62d99755f0b7772a5d3aed03efe27d6546
Vlad Shalandin <vladislav.shalandin@gmail.com>
2022-05-29 19:57:49 -0400

    Set up Review component. Insert fav icons from react-icons library in it

--- src/Review.js
+++ src/Review.js
@@ -3,7 +3,28 @@ import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
-  return <h2>review component</h2>;
+  return (
+    <article className="review">
+      <div className="img-container">
+        <img src={people[0].image} alt="" className="person-img" />
+        <span className="quote-icon">
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4 className="author">name</h4>
+      <p className="job">job</p>
+      <p className="info">text</p>
+      <div className="button-container">
+        <button className="prev-btn">
+          <FaChevronLeft />
+        </button>
+        <button className="next-btn">
+          <FaChevronRight />
+        </button>
+      </div>
+      <button className="random-btn">surprise me</button>
+    </article>
+  );
 };
 
 export default Review;
